85df8299824a6e4c598ff2f3bc383925f381bd5f
moacirrochadev <empreendedormoacirneto@gmail.com>
2026-07-24 01:24:01 -0300

    Build a screen

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,18 +1,43 @@
 import { Pressable, StyleSheet, Text, View } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 type Props = {
     label: string
+    theme?: 'primary'
 }
 
-export default function Button({ label }: Props) {
+export default function Button({ label, theme }: Props) {
+
+    if (theme === 'primary')
+
+        return (
+            <View
+                style={[
+                    styles.buttonContainer,
+                    { borderWidth: 4, borderColor: '#ffd33d', borderRadius: 18 }
+                ]}>
+                <Pressable
+                    style={[
+                        styles.button,
+                        { backgroundColor: '#fff' }]}
+                    onPress={() => alert('You pressed the button!')}>
+                    <FontAwesome name="picture-o" size={18} color="#25292e" style={styles.buttonIcon} />
+                    <Text style={[styles.buttonLabel, { color: '#25292e' }]}>{label}</Text>
+                </Pressable>
+            </View>
+        )
+
     return (
         <View style={styles.buttonContainer}>
-            <Pressable style={styles.buttton} onPress={() => alert('You pressed the button!')}>
+            <Pressable style={styles.button} onPress={() => alert('You pressed a button.')}>
                 <Text style={styles.buttonLabel}>{label}</Text>
             </Pressable>
         </View>
-    )
+    );
+
 }
+
+
 
 const styles = StyleSheet.create({
     buttonContainer: {
@@ -23,7 +48,7 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         padding: 3,
     },
-    buttton: {
+    button: {
         borderRadius: 10,
         width: "100%",
         height: "100%",
@@ -34,5 +59,8 @@ const styles = StyleSheet.create({
     buttonLabel: {
         color: "#fff",
         fontSize: 16
+    },
+    buttonIcon: {
+        paddingRight: 8,
     }
 })
